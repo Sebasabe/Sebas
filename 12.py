e61d521db08cb9dae4371f8ddd06ecfa3b6363e4
Sebasabe <87951736+Sebasabe@users.noreply.github.com>
2021-07-25 19:10:35 -0500

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,161 @@
+print("=========================")
+print(" ┈•※⊰Bienvenidod⊱※┈")
+print("=========================")
+
+aves = ("hola\nantes que respondas el 'hola' di 'ok'\nmensaje: ")
+
+dex = ""
+
+while dex != "hola":
+  dex = input(aves)
+  
+  print(dex)
+
+palabra = input("Bueno pues si saliste del bucle mmmm no soy un bot pero tengo funsiones como \n❂ resta\n❂ musica\n i si quieres seguir di 'hola'\nmensaje: ")  
+  
+if palabra == "musica":
+  print("\nYo te conocí en primavera Me miraste tú de primera De un verano eterno\n me enamora Y esa despedida en septiembre En octubre sí que se siente Noviembre sin ti me dolió también")
+  print("\nLlegará diciembre, sigues en mi mente Fueron seis meses y por fin volveré\n a verte Llegará febrero, yo seré el primero En darte flores y decirte que te quiero Puede que pase un año más de una vez")
+  print("\nSin que te pueda ver Pero el amor es más fuerte Puede que el tiempo nos \naleje otra vez Sin saber dónde estés Pero el amor es más fuerte Uh oh oh oh, oh-oh, oh oh Te esperaré porque el amor es más fuerte (hmm) Ya le dije a mis amigos")
+  print("\nYo no necesito a nadie, solo tú estás en mi mente Mil kilómetros se restan\n cuando dos almas se suman A lo lejos puedo ver Solo pienso, solo pienso en ti Solo pienso, solo pienso en ti Ya mis primos saben tu apellido Que por ti yo estoy perdido Espero que también te pase a ti")
+  print("\nLlegará diciembre, sigues en mi mente Fueron seis meses y por fin volveré a verte Llegará febrero, yo seré el primero En darte flores y decirte que te\n quiero Puede que pase un año más de una vez Sin que te pueda ver Pero el amor es más fuerte Puede que el tiempo nos aleje otra vez Sin saber dónde estés Pero el amor es más fuerte")
+  print("\nOh oh oh oh, oh oh, oh oh Te esperaré porque el amor es más fuerte Oh oh oh\n oh, oh oh, oh oh Te esperaré porque el amor es más fuerte Yo te conocí en primavera Me miraste tú de primera")
+  print("\n espero que la alla leido ok no")
+  print("\n")
+  mor = input("i como te atratado la vidad\nmensaje:")
+  
+  mono = mor[1:4]
+  print(mono)
+  
+  print("\nhe?")
+  lol = input("mm escribe bien\nmensaje: ")
+  
+  mam = lol[1:2]
+  print(mam)
+  
+  print("en fin estas personas de hoy en dia\n pos ok chao lindo dia o noche")
+  
+elif palabra == "resta" or palabra == "suma":
+   
+   cor = "calculadora Sebas* "
+
+   while cor != "hola":
+     print(cor)
+  
+  
+     mar = int(input("primer numero: "))
+
+
+     if mar == 0:
+        print("ella te dejo por culero") 
+  
+     elif mar == 1:
+        print("otro le lleva el desayuno")
+  
+     elif mar == 2:
+        print("no son mas que amigos")
+
+     elif mar == 3:
+        print("te quiso por interes ")
+    
+     elif mar == 4:
+        print("para ella eres un espanto")
+    
+     elif mar == 5:
+        print("eres mas feo que un ornitorrinco")
+    
+     elif mar == 6:
+        print("es inposible que os ameis")
+  
+     elif mar == 7:
+        print("no le sirves ni como juguete")
+    
+     elif mar == 8:
+        print("por desir que te ama le cresio la nariz a pinocho")
+    
+     elif mar == 9:
+        print("de su vida ella te mueve")
+    
+     elif mar == 10:
+        print("con otro la ves")
+    
+     elif mar == 20:
+        print("caiste en sus mentiras siegamente")
+    
+     elif mar == 30:
+        print("otro men la alienta")
+    
+     elif mar == 40:
+        print("ser novio de ella intenta")
+     
+     elif mar == 50:
+        print("tu corazon ella revienta")
+    
+     elif mar == 60:
+        print("en otras piernas ella se sienta")
+    
+     elif mar == 70:
+        print("con otro men ella esta contenta")
+    
+     elif mar == 80:
+        print("la quieres aunque te mienta")
+    
+     elif mar == 100:
+        print("ella no te amo i lo sabes bien")
+    
+
+     else:
+       print("mmm haora si serios")
+  
+       primer = int(input("primer numero: "))
+       segundo = int(input("segundo numero: "))
+       print (" suma es:", primer + segundo)
+       print (" resta es:", primer - segundo)
+       print (" multiplicacion es:", primer * segundo)
+       print (" division es:", primer / segundo)
+ 
+   
+ 
+elif palabra == "hola" or "oa" or "Hola" or "Buenos días" or "culiao" or "wenas" or "a":
+   tur = input("como estas\nmensaje: ")
+   
+   if tur == "bien y tu":
+     print("bien me alegro\n")
+     input("y que tal la familia\nmensaje: ")
+     print("mmm ok\n")
+     print("saves algo he?")
+     print("la vidad te golpea pero te levantas probable mente")
+     print("no se amos lo que queremos")
+     input("\nmensaje: ")
+     print("mmm ok chao lindo dia o noche")
+      
+   elif tur == "bien":
+     print("que bueno me alegro")
+     input("mmm bueno y que haces\nmensaje: ")
+     print("no tengo mas para agregar en vuestra conbersasion\npero podes volver al principio")
+     
+   else:
+     input("y que haces\nmensaje: ")
+     print("haaa ok que bueno\n")
+     input("alguna cosa que quieras preguntar?\nmensaje: ")
+     
+     frase = "\nError mm naaa"
+     for letras in frase:
+       print(letras)
+       
+     print("\nno respondo preguntas\n")
+     input("quieres enamorar o volver a gustarle a alguien?\nmensaje: ")
+     print("bueno mmm, solo se su vida")
+     
+     
+else:
+  input("y que haces\nmensaje: ")
+  print("haaa ok que bueno\n ")
+  input("alguna cosa que quieras preguntar?\nmensaje: ")
+    
+  frase = "\nError mm naaa"
+  for letras in frase:
+    print(letras)
+       
+  print("\nno respondo preguntas\n") 
+  print("mm no soy bot pero tengo una funsiones\nresta\n")
